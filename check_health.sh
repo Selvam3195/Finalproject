646bb43dfd380147038c18485e87f7d7d68bf54b
Selvam3195 <selva3195@gmail.com>
2025-08-25 10:28:08 +0530

    Update check_health.sh

--- check_health.sh
+++ check_health.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
+URL="http://<EC2_PUBLIC_IP>:3000"
 
-# Replace with your EC2 public IP or DNS
-APP_URL="http://<EC2_PUBLIC_IP>:3000"
-
-# Try to fetch the page
-HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" $APP_URL)
-
-if [ "$HTTP_STATUS" -ne 200 ]; then
-  echo "Application is DOWN!"
-  exit 1   # exit with non-zero to fail Jenkins stage
-else
+if curl -s --head "$URL" | grep "200 OK" > /dev/null; then
   echo "Application is UP!"
+  exit 0
+else
+  echo "Application is DOWN!"
+  exit 1
 fi
